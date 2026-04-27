01cdd94c4c2be69d3f6a393a14b7bbcf1cec1123
DJ-Joshi11 <infojoshidevansh11@gmail.com>
2026-04-27 20:46:22 +0530

    Devansh added UC11

--- APP/src/MAIN/QuantityMeasurementAppMAIN.java
+++ APP/src/MAIN/QuantityMeasurementAppMAIN.java
@@ -64,6 +64,34 @@ enum WeightUnit implements IMeasurable {
     }
 }
 
+enum VolumeUnit implements IMeasurable {
+    LITRE(1.0),
+    MILLILITRE(0.001),
+    GALLON(3.78541);
+
+    private final double factor;
+
+    VolumeUnit(double factor) {
+        this.factor = factor;
+    }
+
+    public double getConversionFactor() {
+        return factor;
+    }
+
+    public double convertToBaseUnit(double value) {
+        return value * factor;
+    }
+
+    public double convertFromBaseUnit(double baseValue) {
+        return baseValue / factor;
+    }
+
+    public String getUnitName() {
+        return name();
+    }
+}
+
 class Quantity<U extends IMeasurable> {
     private final double value;
     private final U unit;
@@ -123,16 +151,10 @@ class Quantity<U extends IMeasurable> {
 
 public class QuantityMeasurementAppMAIN {
     public static void main(String[] args) {
-        Quantity<LengthUnit> l1 = new Quantity<>(1.0, LengthUnit.FEET);
-        Quantity<LengthUnit> l2 = new Quantity<>(12.0, LengthUnit.INCHES);
-        System.out.println(l1.equals(l2));
-        System.out.println(l1.add(l2, LengthUnit.FEET));
-        System.out.println(l1.convertTo(LengthUnit.INCHES));
-
-        Quantity<WeightUnit> w1 = new Quantity<>(1.0, WeightUnit.KILOGRAM);
-        Quantity<WeightUnit> w2 = new Quantity<>(1000.0, WeightUnit.GRAM);
-        System.out.println(w1.equals(w2));
-        System.out.println(w1.add(w2, WeightUnit.KILOGRAM));
-        System.out.println(w1.convertTo(WeightUnit.GRAM));
+        Quantity<VolumeUnit> v1 = new Quantity<>(1.0, VolumeUnit.LITRE);
+        Quantity<VolumeUnit> v2 = new Quantity<>(1000.0, VolumeUnit.MILLILITRE);
+        System.out.println(v1.equals(v2));
+        System.out.println(v1.convertTo(VolumeUnit.MILLILITRE));
+        System.out.println(v1.add(v2, VolumeUnit.LITRE));
     }
 }
